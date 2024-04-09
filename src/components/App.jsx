cb3c138a5eb32a77c95c7d67d126a63f40eaa3da
Michelle0805 <vasudaria85@gmail.com>
2024-04-09 12:56:20 +0800

    Added codes and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
+import React, { useState} from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+  const [contacts, setContacts] = useState ([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = newContact => {
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+    
+
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+     prevContacts.filter(contact => contact.id !== id),
+    );
+  };
+ 
+
+  const filterContact = () => {
+    const filterLowerCase = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterLowerCase)
+    );
+  };
+ 
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={addContact} contacts={contacts}/>
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} setFilter={setFilter} />
+        <ContactList
+          filterContact={filterContact}
+          deleteContact={deleteContact}
+        />
+      </div>
+    );
+  };
+
